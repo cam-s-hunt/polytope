0231ddf19f04e223543c7676637cc69c77d27697
cam0sum <cam@0sum.club>
2019-08-01 15:13:05 -0400

    commented out magic variables

--- polytope_install.sh
+++ polytope_install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit;
 set -o nounset;
+set -o pipefail;
 # set -o xtrace
 
 # Set magic variables for current file & dir
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")";
-__base="$(basename ${__file} .sh)";
-__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app;
+#__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+#__file="${__dir}/$(basename "${BASH_SOURCE[0]}")";
+#__base="$(basename ${__file} .sh)";
+#__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app;
 
-arg1="${1:-}";
+#arg1="${1:-}";
 
 
 # $(command) instead of `command`
